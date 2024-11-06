7fc213a8557577de0fae901c9c8986786222a15d
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2024-11-06 23:44:44 +0100

    Make image-upload nicer

--- code/projects/app/src/app/shared/image-upload/image-upload.component.ts
+++ code/projects/app/src/app/shared/image-upload/image-upload.component.ts
@@ -11,19 +11,59 @@ import { fromFile } from "../../util/image-data";
 export class ImageUploadComponent {
   @Output() public readonly imageChange = new EventEmitter<ImageData>();
 
+  protected readonly inputId = crypto.randomUUID();
   protected readonly supportedMimeTypes = [
     "image/jpeg",
     "image/png",
     "image/webp",
   ];
 
-  protected onFileChange(event: Event): void {
+  protected isHovered = false;
+  protected isFlashRed = false;
+  protected isFlashGreen = false;
+
+  protected cancelEvent(event: Event): void {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  protected onDragOver(event: DragEvent): void {
+    this.cancelEvent(event);
+    this.isHovered = true;
+  }
+
+  protected onDragLeave(event: DragEvent): void {
+    this.cancelEvent(event);
+    this.isHovered = false;
+  }
+
+  protected onDrop(event: DragEvent): void {
+    this.cancelEvent(event);
+    this.isHovered = false;
+    this.handleFile(event.dataTransfer?.files?.[0]);
+  }
+
+  protected onInputChange(event: Event): void {
+    this.cancelEvent(event);
     const input = event.currentTarget;
     if (!(input instanceof HTMLInputElement)) return;
+    this.handleFile(input.files?.[0]);
+  }
 
-    const file = input.files?.[0];
-    if (!file || !this.supportedMimeTypes.includes(file.type)) return;
+  private handleFile(file: File | null | undefined): void {
+    this.isFlashRed = false;
+    this.isFlashGreen = false;
 
-    fromFile(file).then((image) => this.imageChange.emit(image));
+    requestAnimationFrame(() => {
+      if (!file || !this.supportedMimeTypes.includes(file.type)) {
+        this.isFlashRed = true;
+        return;
+      }
+
+      fromFile(file).then((image) => {
+        this.imageChange.emit(image);
+        this.isFlashGreen = true;
+      });
+    });
   }
 }
