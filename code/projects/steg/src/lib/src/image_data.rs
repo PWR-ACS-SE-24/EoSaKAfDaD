036cc00f094438639dd76d23d5bb2f1cc8f7c5ce
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2024-11-21 10:41:36 +0100

    Add upload and download button styling

--- code/projects/steg/src/lib/src/image_data.rs
+++ code/projects/steg/src/lib/src/image_data.rs
@@ -1,7 +1,6 @@
-use wasm_bindgen::Clamped;
-use web_sys::ImageData;
+use web_sys::{js_sys::Uint8ClampedArray, ImageData};
 
 pub fn create_image_data(data: &[u8], width: u32) -> ImageData {
-    ImageData::new_with_u8_clamped_array(Clamped(data), width)
+    ImageData::new_with_js_u8_clamped_array(&Uint8ClampedArray::from(data), width)
         .expect("ImageData constructor should succeed")
 }
