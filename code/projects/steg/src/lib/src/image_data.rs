bf11ced004c40bc3bbbd3bc8a43a0f99a9f1e8b9
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2024-11-22 12:41:59 +0100

    Refactor tool.rs

--- code/projects/steg/src/lib/src/image_data.rs
+++ code/projects/steg/src/lib/src/image_data.rs
@@ -1,5 +1,7 @@
 use web_sys::{js_sys::Uint8ClampedArray, ImageData};
 
+pub const RGBA_CHANNELS: usize = 4;
+
 pub fn create_image_data(data: &[u8], width: u32) -> ImageData {
     ImageData::new_with_js_u8_clamped_array(&Uint8ClampedArray::from(data), width)
         .expect("ImageData constructor should succeed")
@@ -7,6 +9,7 @@ pub fn create_image_data(data: &[u8], width: u32) -> ImageData {
 
 pub trait ImageDataExt {
     fn xy_to_idx(&self, x: u32, y: u32) -> usize;
+    fn map_rgb(&self, f: impl FnMut(u8) -> u8) -> ImageData;
 }
 
 impl ImageDataExt for ImageData {
@@ -14,5 +17,16 @@ impl ImageDataExt for ImageData {
         let x = x.clamp(0, self.width() - 1);
         let y = y.clamp(0, self.height() - 1);
         ((y * self.width() + x) * 4) as usize
+    }
+
+    fn map_rgb(&self, mut f: impl FnMut(u8) -> u8) -> ImageData {
+        let mut data = Vec::with_capacity(self.data().len());
+        for i in (0..self.data().len()).step_by(RGBA_CHANNELS) {
+            data.push(f(self.data()[i]));
+            data.push(f(self.data()[i + 1]));
+            data.push(f(self.data()[i + 2]));
+            data.push(self.data()[i + 3]);
+        }
+        create_image_data(&data, self.width())
     }
 }
--- code/projects/steg/src/lib/src/tool.rs
+++ code/projects/steg/src/lib/src/tool.rs
@@ -1,9 +1,7 @@
-use crate::image_data::{create_image_data, ImageDataExt};
+use crate::image_data::{create_image_data, ImageDataExt, RGBA_CHANNELS};
 use tinyrand::{Rand, Seeded, StdRand};
 use wasm_bindgen::prelude::*;
 use web_sys::ImageData;
-
-const RGBA_CHANNELS: usize = 4;
 
 #[wasm_bindgen(js_name = toolScale)]
 pub fn tool_scale(image: &ImageData, scale: u32) -> ImageData {
@@ -28,43 +26,21 @@ pub fn tool_scale(image: &ImageData, scale: u32) -> ImageData {
 pub fn tool_noise(image: &ImageData, amount: u32) -> ImageData {
     let amount = amount.clamp(0, 100);
     let mut rng = StdRand::seed(amount as u64);
-    let mut data = Vec::with_capacity(image.data().len());
-    for i in 0..image.data().len() {
+    image.map_rgb(|v| {
         let noise = rng.next_u32() & 0xFF;
-        let value = image.data()[i] as u32;
-        let blend = (value * (100 - amount) + noise * amount) / 100;
-        data.push(blend as u8);
-    }
-    create_image_data(&data, image.width())
+        ((v as u32 * (100 - amount) + noise * amount) / 100) as u8
+    })
 }
 
 #[wasm_bindgen(js_name = toolBrightness)]
 pub fn tool_brightness(image: &ImageData, change: i32) -> ImageData {
     let change = change.clamp(-255, 255);
-    let mut data = Vec::with_capacity(image.data().len());
-    for i in (0..image.data().len()).step_by(RGBA_CHANNELS) {
-        for j in 0..3 {
-            let value = image.data()[i + j] as i32;
-            let new_value = (value + change) as u8;
-            data.push(new_value);
-        }
-        data.push(image.data()[i + 3]);
-    }
-    create_image_data(&data, image.width())
+    image.map_rgb(|v| (v as i32 + change).clamp(0, 255) as u8)
 }
 
 #[wasm_bindgen(js_name = toolContrast)]
 pub fn tool_contrast(image: &ImageData, change: i32) -> ImageData {
     let change = change.clamp(-255, 255);
     let factor = (259. * (change as f64 + 255.)) / (255. * (259. - change as f64));
-    let mut data = Vec::with_capacity(image.data().len());
-    for i in (0..image.data().len()).step_by(RGBA_CHANNELS) {
-        for j in 0..3 {
-            let value = image.data()[i + j] as f64;
-            let new_value = (factor * (value - 128.) + 128.).clamp(0., 255.) as u8;
-            data.push(new_value);
-        }
-        data.push(image.data()[i + 3]);
-    }
-    create_image_data(&data, image.width())
+    image.map_rgb(|v| (factor * (v as f64 - 128.) + 128.) as u8)
 }
