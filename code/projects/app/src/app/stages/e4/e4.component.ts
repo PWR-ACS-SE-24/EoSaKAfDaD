4eecf7fb473a678c654dd796e72da2a29220300b
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2024-11-27 17:23:17 +0100

    Cache layers of image transformations (thx @mlodybercik)

--- code/projects/app/src/app/stages/e4/e4.component.ts
+++ code/projects/app/src/app/stages/e4/e4.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { BehaviorSubject, combineLatest, map, Subject } from "rxjs";
+import { BehaviorSubject, combineLatestWith, map, share, Subject } from "rxjs";
 import { ImageDisplayComponent } from "../../shared/image-display/image-display.component";
 import { ImageUploadComponent } from "../../shared/image-upload/image-upload.component";
 import { ImageDownloadComponent } from "../../shared/image-download/image-download.component";
@@ -25,32 +25,19 @@ export class E4Component {
   protected readonly noiseSubject = new BehaviorSubject(0);
   protected readonly brighnessSubject = new BehaviorSubject(0);
   protected readonly contrastSubject = new BehaviorSubject(0);
-  private readonly change$ = combineLatest([
-    this.scaleSubject,
-    this.noiseSubject,
-    this.brighnessSubject,
-    this.contrastSubject,
-  ]).pipe(
-    map(([scale, noise, brightness, contrast]) => ({
-      scale,
-      noise,
-      brightness,
-      contrast,
-    }))
-  );
 
   private readonly imageSubject = new Subject<ImageData>();
   protected readonly oldImage$ = this.imageSubject.asObservable();
-  protected readonly newImage$ = combineLatest([
-    this.oldImage$,
-    this.change$,
-  ]).pipe(
-    map(([image, { scale, noise, brightness, contrast }]) =>
-      toolContrast(
-        toolBrightness(toolNoise(toolScale(image, scale), noise), brightness),
-        contrast
-      )
-    )
+  protected readonly newImage$ = this.oldImage$.pipe(
+    combineLatestWith(this.scaleSubject),
+    map(([i, scale]) => toolScale(i, scale)),
+    combineLatestWith(this.noiseSubject),
+    map(([i, noise]) => toolNoise(i, noise)),
+    combineLatestWith(this.brighnessSubject),
+    map(([i, brightness]) => toolBrightness(i, brightness)),
+    combineLatestWith(this.contrastSubject),
+    map(([i, contrast]) => toolContrast(i, contrast)),
+    share()
   );
 
   protected dimensions(image: ImageData | null): string | null {
