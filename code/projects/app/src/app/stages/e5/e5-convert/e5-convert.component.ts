af9cccdef34cf2ee75e5384cf91548cb6b5809a5
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2024-11-29 16:20:05 +0100

    Convert images to grayscale

--- code/projects/app/src/app/stages/e5/e5-convert/e5-convert.component.ts
+++ code/projects/app/src/app/stages/e5/e5-convert/e5-convert.component.ts
@@ -1,9 +1,29 @@
 import { Component } from "@angular/core";
+import { map, Subject } from "rxjs";
+import { vcMakeMonochrome } from "steg";
+import { ImageDisplayComponent } from "../../../shared/image-display/image-display.component";
+import { ImageUploadComponent } from "../../../shared/image-upload/image-upload.component";
+import { ImageDownloadComponent } from "../../../shared/image-download/image-download.component";
 
 @Component({
   selector: "app-e5-convert",
   standalone: true,
   templateUrl: "./e5-convert.component.html",
   styleUrl: "./e5-convert.component.css",
+  imports: [
+    ImageDisplayComponent,
+    ImageUploadComponent,
+    ImageDownloadComponent,
+  ],
 })
-export class E5ConvertComponent {}
+export class E5ConvertComponent {
+  private readonly imageSubject = new Subject<ImageData>();
+  protected readonly oldImage$ = this.imageSubject.asObservable();
+  protected readonly newImage$ = this.oldImage$.pipe(
+    map((image) => vcMakeMonochrome(image, "average", "random"))
+  );
+
+  protected onImageUpload(image: ImageData): void {
+    this.imageSubject.next(image);
+  }
+}
--- code/projects/steg/src/lib/src/vc.rs
+++ code/projects/steg/src/lib/src/vc.rs
@@ -1,1 +1,41 @@
+use crate::image_data::{create_image_data, RGBA_CHANNELS};
+use wasm_bindgen::prelude::*;
+use web_sys::ImageData;
 
+#[wasm_bindgen]
+#[derive(Clone, Copy, Debug)]
+pub enum VcGrayscale {
+    Average = "average",
+    Luminosity = "luminosity",
+}
+
+#[wasm_bindgen]
+#[derive(Clone, Copy, Debug)]
+pub enum VcDithering {
+    Threshold = "threshold",
+    Random = "random",
+    FloydSteinberg = "floyd-steinberg",
+}
+
+#[wasm_bindgen(js_name = vcMakeMonochrome)]
+pub fn vc_make_monochrome(
+    image: &ImageData,
+    grayscale: VcGrayscale,
+    dithering: VcDithering,
+) -> ImageData {
+    let mut data = Vec::with_capacity(image.data().len());
+    for i in (0..image.data().len()).step_by(RGBA_CHANNELS) {
+        let r = image.data()[i] as f64;
+        let g = image.data()[i + 1] as f64;
+        let b = image.data()[i + 2] as f64;
+
+        let gray = match grayscale {
+            VcGrayscale::Average => 0.333 * r + 0.333 * g + 0.333 * b,
+            VcGrayscale::Luminosity => 0.299 * r + 0.587 * g + 0.114 * b,
+            _ => unreachable!(),
+        } as u8;
+
+        data.extend_from_slice(&[gray, gray, gray, u8::MAX]);
+    }
+    create_image_data(&data, image.width())
+}
